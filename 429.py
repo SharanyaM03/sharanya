314aafafdaba7171ea73b287672f9e8178342c86
SharanyaM03 <sharanyamalla15@gmail.com>
2025-03-26 10:36:49 +0530

    Add files via upload

---
+++ 429.py
@@ -0,0 +1,16 @@
+def get_person_details():
+    name=input("Enter your name:")
+    address=input("Enter your address:")
+    email=input("Enter your email:")
+    phone=input("Enter your phone number:")
+    return name,address,email,phone
+def print_person_details(name,address,email,phone):
+    print("\n--- Personal Details ---")
+    print(f"Name:{name}")
+    print(f"Address:{address}")
+    print(f"Email:{email}")
+    print(f"Phone Number:{phone}")
+#Get details from the user
+name,address,email,phone=get_person_details()
+#Print the details
+print_person_details(name,address,email,phone)
